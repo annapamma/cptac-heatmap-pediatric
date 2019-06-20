c0e3bf4ed69323dabee88b1e350839d7d4756455
Anna Calinawan <anna.pamela@gmail.com>
2019-06-20 12:35:53 -0400

    adds logo container

---
+++ client/src/components/LogoContainer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="logo-container">
+   <div class="cptac-logo">
+        <img src="@/assets/cptac-logo.png">
+    </div>
+    <div class="mssm-logo">
+        <img src="@/assets/MSSMLogo.png">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  name: 'LogoContainer',
+};
+</script>
+
+<style scoped>
+  .logo-container {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    /*height: 20%;*/
+    margin: auto;
+    padding: 5px;
+    /*background-color: purple;*/
+    justify-content: space-between;
+
+  }
+</style>
