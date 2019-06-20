9ec6b666b5d2a208c269bf98f83e94a859c3897e
Anna Calinawan <anna.pamela@gmail.com>
2019-06-20 12:28:01 -0400

    adds root vue file

---
+++ client/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <div class="logo-bar">
+      <logo-container />
+    </div>
+    <div class="main">
+      <!--<input-container />-->
+      <heatmap-container />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import InputContainer from '@/components/InputContainer.vue';
+import HeatmapContainer from '@/components/HeatmapContainer.vue';
+import LogoContainer from '@/components/LogoContainer.vue';
+
+export default {
+  components: {
+    HeatmapContainer,
+    // InputContainer,
+    LogoContainer,
+  },
+};
+</script>
+
+<style scoped>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    /*text-align: center;*/
+    color: #2c3e50;
+    width: 99vw;
+    height: 100vh;
+    margin: 0 auto;
+  }
+
+  .main {
+    display: flex;
+    flex-direction: row;
+    height: 85%;
+  }
+
+  .logo-bar {
+    display: flex;
+    flex-direction: row;
+    height: 80px;
+  }
+</style>
