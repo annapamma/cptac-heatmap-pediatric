988a63e8beae2c5e02e10e59af285a3016a5dd87
Anna Calinawan <anna.pamela@gmail.com>
2019-06-23 19:26:21 -0400

    moves disease buttons to top

--- client/src/views/DiseaseAll.vue
+++ client/src/views/DiseaseAll.vue
@@ -84,18 +84,18 @@ export default {
     },
   },
   mounted() {
-    let legendImg = document.createElement("img");
-    legendImg.src = require("@/assets/legend.jpg");
-    legendImg.style = "background-color: #00FFFF; " +
-      "max-width: 1000px; " +
-      "min-width: 400px; " +
-      "text-align: center; " +
-      "display: block; " +
-      "margin-left: auto; " +
-      "margin-right: auto;";
-    let parentNode = document.querySelector('.disease-all');
+    const legendImg = document.createElement('img');
+    legendImg.src = require('@/assets/legend.jpg');
+    legendImg.style = 'background-color: #00FFFF; '
+      + 'max-width: 1000px; '
+      + 'min-width: 400px; '
+      + 'text-align: center; '
+      + 'display: block; '
+      + 'margin-left: auto; '
+      + 'margin-right: auto;';
+    const parentNode = document.querySelector('.disease-all');
     parentNode.append(legendImg);
-  }
+  },
 };
 </script>
 
